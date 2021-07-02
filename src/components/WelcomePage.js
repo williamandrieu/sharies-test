6af8de47ec5a5d5f9762294087a8c31020829907
Maxime <maximeld19@gmail.com>
2021-07-02 03:34:52 +0200

    logowhite

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,15 +3,16 @@ import { auth } from '../firebase'
 import { setTitle } from '../util'
 
 import avatar from '../images/avatar.png'
+import logosharieswhite from '../images/logosharieswhite.svg'
 
 const menuItemList = [
-   {title:"Profile"},
-   {title:"Documents" },
-   {title:"Factures"},
-   {title:"Informations"},
-   {title:"Services"},
-   {title:"Reservation"},
-   {title:"Messagerie"}
+    { title: "Profile" },
+    { title: "Documents" },
+    { title: "Factures" },
+    { title: "Informations" },
+    { title: "Services" },
+    { title: "Reservation" },
+    { title: "Messagerie" }
 ]
 
 export const WelcomePage = (props) => {
@@ -27,24 +28,43 @@ export const WelcomePage = (props) => {
     }, [])
 
     return (
-        <div>
-            {auth.currentUser.email}
-            <div onClick={signOut}>Déconnection</div>
+        <div className={"welcome-wrapper"}>
             <LeftMenu>
-                {menuItemList.map((item,index) => (<LeftMenuItem onClick={()=>setCurrentState(index)} isSelected={index === currentState} title={item.title}/>))}
+                <img className={'left-menu-logo'} src={logosharieswhite}/>
+                {menuItemList.map((item, index) => (<LeftMenuItem onClick={() => setCurrentState(index)} isSelected={index === currentState} title={item.title} />))}
+                <div className={'disconnection-btn'} onClick={signOut}>Déconnexion</div>
             </LeftMenu>
             <div>
-                <div>{menuItemList[currentState].title}</div>
-                <img src={auth.currentUser.photoURL ?? avatar}/>
-                <div>Bienvenue {auth.currentUser.displayName ?? auth.currentUser.email}</div>
+                {currentState == 0 && <Profile/>}
+                {currentState != 0 && <NoContaint title={menuItemList[currentState].title}/>}
             </div>
+        </div>
+    )
+}
+
+const Profile = () => {
+    return (
+        <div>
+            <img className={'profile-image'} src={auth.currentUser.photoURL ?? avatar} />
+            <div>Bonjour
+                <div>{auth.currentUser.displayName ?? auth.currentUser.email}</div>
+            </div>
+        </div>
+    )
+}
+
+const NoContaint = (props) => {
+    return (
+        <div>
+            {props.title}
+            <div>Cette page sera bientot disponible</div>
         </div>
     )
 }
 
 const LeftMenu = (props) => {
     return (
-        <div>
+        <div className={'left-menu'} >
             {props.children}
         </div>
     )
@@ -52,7 +72,7 @@ const LeftMenu = (props) => {
 
 const LeftMenuItem = (props) => {
     return (
-        <div onClick={props.onClick}>
+        <div onClick={props.onClick} className={`left-menu-item ${props.isSelected ? "selected" : ""}`}>
             {props.title}
         </div>
     )
